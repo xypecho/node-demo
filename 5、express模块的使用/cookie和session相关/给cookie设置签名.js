45e940cc93f4550366d70307b129fce7730e4015
xypecho <xypecho@163.com>
2018-07-13 21:35:25 +0800

    update

--- 5、express模块的使用/cookie和session相关/给cookie设置签名.js
+++ 5、express模块的使用/cookie和session相关/给cookie设置签名.js
@@ -7,7 +7,8 @@ app.use(cookieParse());
 app.use('/', (req, res) => {
 	req.secret = 'sdasdlaksjdkajsdl'; // 设置签名
 	res.cookie('name', 'test', {signed: true}); // 来校验cookie是否被篡改过
-	console.log(req.cookies); //{ name: 'test' }
+	console.log(req.cookies); //只能获取未设置签名的cookie
+	console.log(req.signedCookies); // 获取已经被签名的cookie
 	res.send('hello world');
 })
 
