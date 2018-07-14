f815f72f62df2d59f537bd55d00c1c4c38c6ad0b
xypecho <xypecho@163.com>
2018-07-14 15:06:24 +0800

    学习jade模版引擎

--- 6、jade模版引擎/模版引擎内写标签属性以及css样式/server.js
+++ 6、jade模版引擎/模版引擎内写标签属性以及css样式/server.js
@@ -1,4 +1,12 @@
 const jade = require('jade');
+const fs = require('fs');
 
 const str = jade.renderFile('./index.jade', {pretty: true})
 console.log(str);
+fs.writeFile('./test.html', str, (err, date) => {
+	if (err) {
+		console.log('创建文件错误')
+	} else {
+		console.log('success');
+	}
+})
