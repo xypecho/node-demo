4eb5c9e311e5513c5b2e6ea4b8f4301ca2e1c3c0
xypecho <xypecho@163.com>
2018-07-14 21:42:21 +0800

    学习jade模版引擎

--- 6、jade模版引擎/循环输出/server.js
+++ 6、jade模版引擎/循环输出/server.js
@@ -1,4 +1,4 @@
 const jade = require('jade');
 
-const str = jade.renderFile('./index.jade', {pretty: true, arr: ['a', 'b', 'c', 'd']});
+	const str = jade.renderFile('./index.jade', {pretty: true, arr: ['a', 'b', 'c', 'd'], text:'<h1>这是文档啊啊啊</h1>'});
 console.log(str);
