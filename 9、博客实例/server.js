3b9bdd4d07a42f559b98a27db81c30c88a31d843
xypecho <xypecho@163.com>
2018-07-20 21:43:47 +0800

    博客实例

--- 9、博客实例/server.js
+++ 9、博客实例/server.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const expressStatic = require('express-static');
+const cookieParser=require('cookie-parser');
+const cookieSession=require('cookie-session');
+const bodyParser=require('body-parser');
+const multer=require('multer');
+const consolidate=require('consolidate');
 const mysql = require('mysql');
 
 //创建连接池
@@ -6,6 +12,11 @@ const db = mysql.createPool({host: 'localhost', user: 'root', password: '123456'
 
 const app = express();
 app.listen('8080');
+
+// 配置模版引擎
+app.set('view engine', 'html'); // 设置输出的文本格式
+app.set('views','./views');
+app.engine('html',consolidate.ejs); // 使用哪种模版引擎
 
 app.use('/', (req, res) => {
 	db.query('SELECT * FROM banner_table',(err, data) => {
