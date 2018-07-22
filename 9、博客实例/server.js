3b68ca2f50329a4f0219a66a7248be45921f8b53
xypecho <xypecho@163.com>
2018-07-22 12:28:02 +0800

    微博实例

--- 9、博客实例/server.js
+++ 9、博客实例/server.js
@@ -13,17 +13,24 @@ const db = mysql.createPool({host: 'localhost', user: 'root', password: '123456'
 const app = express();
 app.listen('8080');
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(multer({dest: './www/upload'}).any());
 // 配置模版引擎
 app.set('view engine', 'html'); // 设置输出的文本格式
-app.set('views','./views');
+app.set('views','./template');
 app.engine('html',consolidate.ejs); // 使用哪种模版引擎
 
-app.use('/', (req, res) => {
-	db.query('SELECT * FROM banner_table',(err, data) => {
-		if (err) {
-			res.send('查询错误')
-		} else {
-			res.send(data)
-		}
-	})
-})
+
+//4.static数据
+app.use(expressStatic('./www'));
+
+
+// app.use('/', (req, res) => {
+// 	db.query('SELECT * FROM banner_table',(err, data) => {
+// 		if (err) {
+// 			res.send('查询错误')
+// 		} else {
+// 			res.render('index.ejs', {banners: data});
+// 		}
+// 	})
+// })
