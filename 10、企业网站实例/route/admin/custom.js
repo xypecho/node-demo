0017e5728af492e03f5f9ec8c111c7ed0f04ae66
xypecho <xypecho@163.com>
2018-08-14 22:28:52 +0800

    企业网站实例

--- 10、企业网站实例/route/admin/custom.js
+++ 10、企业网站实例/route/admin/custom.js
@@ -6,18 +6,49 @@ const router = express.Router();
 
 module.exports = () => {
     router.get('/', (req, res) => {
-        db.query('SELECT * FROM custom_evaluation_table', (err, data) => {
-            if (err) {
-                res.status(500).send('failed to select data from custom_evaluation_table').end();
-            } else {
-                res.render('./admin/custom.ejs');
-            }
-        })
+        // 判断是否是删除数据的请求
+        if (req.query.act == 'delete') {
+            db.query(`DELETE FROM custom_evaluation_table WHERE ID=${req.query.id}`, (err, data) => {
+                if (err) {
+                    res.status(500).send('failed to delete data from custom_evaluation_table')
+                } else {
+                    res.redirect('/admin/custom')
+                }
+            })
+        } else {
+            db.query('SELECT * FROM custom_evaluation_table', (err, data) => {
+                if (err) {
+                    res.status(500).send('failed to select data from custom_evaluation_table').end();
+                } else {
+                    res.render('./admin/custom.ejs', { data });
+                }
+            })
+        }
     })
     router.post('/', (req, res) => {
-        console.log(req.body)
+        // 判断是编辑还是新增，有id则是编辑
+        let title = req.body.title;
+        let description = req.body.description;
+        let src = req.body.src;
+        console.log(`{'${title}','${description}','${src}'}`)
         console.log(req.files)
-        
+        if (req.body.id) {
+            db.query(`UPDATE custom_evaluation_table SET title='${title}',description='${description}',src='${src}' WHERE ID = ${req.body.id}`,(err,data)=>{
+                if (err) {
+                    res.status(500).send('failed to update data in custom_evaluation_table')
+                } else {
+                    res.redirect('/admin/custom')
+                }
+            })
+        } else {
+            db.query(`INSERT INTO custom_evaluation_table (title,description,src) VALUES ('${title}','${description}','${src}')`,(err,data)=>{
+                if (err) {
+                    res.status(500).send('failed to insert data in custom_evaluation_table')
+                } else {
+                    res.redirect('/admin/custom')
+                }
+            })
+        }
     })
     return router;
 }
