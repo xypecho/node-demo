3aabcd7c413074ace5b8b46ccbed254cee3fef59
xypecho <xypecho@163.com>
2018-08-14 23:27:17 +0800

    企业网站实例

--- 10、企业网站实例/route/admin/custom.js
+++ 10、企业网站实例/route/admin/custom.js
@@ -30,29 +30,33 @@ module.exports = () => {
         // 判断是编辑还是新增，有id则是编辑
         let title = req.body.title;
         let description = req.body.description;
-        let src = req.files[0].destination + '/' + new Date().toLocaleString().replace(' ', '-').replace(/:/g, '-') + path.extname(req.files[0].originalname);
+        console.log(req.files[0])
+        console.log(path.parse(req.files[0].path).destination)
+        let src = `/${req.files[0].destination}/${req.files[0].originalname}${path.extname(req.files[0].originalname)}`;
         fs.rename(req.files[0].path, src, (err, data) => {
             if (err) {
-                res.status(500).send('failed to upload files').end();
+                // res.status(500).send('failed to upload files').end();
+                console.log(err)
+            } else {
+                if (req.body.id) {
+                    db.query(`UPDATE custom_evaluation_table SET title='${title}',description='${description}',src='${src}' WHERE ID = ${req.body.id}`, (err, data) => {
+                        if (err) {
+                            res.status(500).send('failed to update data in custom_evaluation_table')
+                        } else {
+                            res.redirect('/admin/custom')
+                        }
+                    })
+                } else {
+                    db.query(`INSERT INTO custom_evaluation_table (title,description,src) VALUES ('${title}','${description}','${src}')`, (err, data) => {
+                        if (err) {
+                            res.status(500).send('failed to insert data in custom_evaluation_table')
+                        } else {
+                            res.redirect('/admin/custom')
+                        }
+                    })
+                }
             }
         })
-        if (req.body.id) {
-            db.query(`UPDATE custom_evaluation_table SET title='${title}',description='${description}',src='${src}' WHERE ID = ${req.body.id}`, (err, data) => {
-                if (err) {
-                    res.status(500).send('failed to update data in custom_evaluation_table')
-                } else {
-                    res.redirect('/admin/custom')
-                }
-            })
-        } else {
-            db.query(`INSERT INTO custom_evaluation_table (title,description,src) VALUES ('${title}','${description}','${src}')`, (err, data) => {
-                if (err) {
-                    res.status(500).send('failed to insert data in custom_evaluation_table')
-                } else {
-                    res.redirect('/admin/custom')
-                }
-            })
-        }
     })
     return router;
 }
