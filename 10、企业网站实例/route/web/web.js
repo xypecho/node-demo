7d669be68ba8fe81f8d79cb505642ec064ab2b2c
xypecho <xypecho@163.com>
2018-08-17 23:09:59 +0800

    update

--- 10、企业网站实例/route/web/web.js
+++ 10、企业网站实例/route/web/web.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express()
 const expressStatic = require('express-static');
+const mysql = require('mysql');
 
 module.exports = () => {
 	const router = express.Router();
-	router.get('/',(req,res) => {
-		res.render('./web/index.html')
+	db = mysql.createPool({ host: 'localhost', post: '3306', user: 'root', password: '123456', database: 'company_website' })
+	router.get('/', (req, res) => {
+		res.redirect('/index.html')
+	})
+	router.get('/get_banners', (req, res) => {
+		db.query('SELECT * FROM banner_table', (err, data) => {
+			if (err) {
+				res.status(500).send('failed to get data').end();
+			} else {
+				res.status(200).send([data]).end();
+			}
+		})
 	})
 	return router;
 }
